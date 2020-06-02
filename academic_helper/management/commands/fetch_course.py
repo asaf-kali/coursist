3fa95eb727d6a57e849017dfd2c46f1d9f43b895
Asaf Kali <akali93@cs.huji.ac.il>
2020-06-02 19:53:06 +0300

    Small fixes to management commands

--- academic_helper/management/commands/fetch_course.py
+++ academic_helper/management/commands/fetch_course.py
@@ -4,11 +4,10 @@ from academic_helper.logic.shnaton_parser import ShnatonParser
 
 
 class Command(BaseCommand):
-
     def add_arguments(self, parser):
-        parser.add_argument('course_number', type=int)
+        parser.add_argument("course_number", type=int)
 
     def handle(self, *args, **options):
-        course_number = options['course_number']
-        print(f'fetching course_number = {course_number}')
+        course_number = options["course_number"]
+        print(f"fetching course_number = {course_number}")
         ShnatonParser.fetch_course(course_number)
