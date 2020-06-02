4c7584cf0b003c742dc1dea71adcbe5573bf9cf5
Asaf Kali <akali93@cs.huji.ac.il>
2020-06-02 23:49:14 +0300

    More sec changes

--- scripts/run_server.sh
+++ scripts/run_server.sh
@@ -1,5 +1,6 @@
-export LD_LIBRARY_PATH="/usr/local/lib"
-
 source ./env/bin/activate
+
+export LD_LIBRARY_PATH="/usr/local/lib"
+export COURSIST_ENV="prod"
 
 python manage.py runserver 0.0.0.0:5000
