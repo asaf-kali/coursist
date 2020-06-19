9b4b4f123b4cea15e30672c1fd4782241d8630b4
evyatar100 <evyatar100>
2020-06-19 11:00:44 +0300

    delete redundant files

--- course_comments/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-from course_comments.models import CourseComment
-
-admin.site.register(CourseComment)
--- course_comments/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CourseCommentsConfig(AppConfig):
-    name = "course_comments"
