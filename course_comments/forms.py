5fc1774ca0dae7a9cb1e6677b44ac8b55408ccdf
evyatar100 <evyatar100>
2020-06-24 23:10:36 +0300

    WIP: make to comments page look a little better.

--- course_comments/forms.py
+++ course_comments/forms.py
@@ -4,6 +4,7 @@ from django_comments.forms import CommentForm
 
 class CourseCommentForm(CommentForm):
     is_anonymous = forms.BooleanField(required=False)
+    comment = forms.CharField(label='Comment', widget=forms.Textarea({'cols': '70', 'rows': '7'}))
 
     def get_comment_create_data(self, site_id=None):
         # Use the data of the superclass, and add in the title field
