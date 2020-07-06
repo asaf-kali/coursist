afa454aef9502738568f007ced1c3facd9e53ba5
Asaf Kali <akali93@cs.huji.ac.il>
2020-07-06 17:11:04 +0300

    Some progress

--- academic_helper/management/commands/restore_raw.py
+++ academic_helper/management/commands/restore_raw.py
@@ -1,11 +1,13 @@
+import re
+from time import sleep
 from typing import List
 
 from django.core import management
 from django.core.management import BaseCommand
 from django.db import connection
-from tqdm.auto import tqdm, trange
+from tqdm.auto import tqdm
 
-from academic_helper.utils.logger import log
+from academic_helper.utils.logger import log, wrap
 
 
 def concat(queries: List[str]) -> List[str]:
@@ -19,6 +21,18 @@ def concat(queries: List[str]) -> List[str]:
     return result
 
 
+insert_re = re.compile(r"INSERT INTO \"(.*)\" VALUES\((.*)\);\s*")
+
+
+def reconstruct_insert(query: str) -> str:
+    result = insert_re.search(query)
+    if not result:
+        return ""
+    table = result.group(1)
+    values = result.group(2)
+    return f'INSERT INTO {table} VALUES ({values});'
+
+
 class Command(BaseCommand):
     def __init__(self):
         super().__init__()
@@ -30,39 +44,43 @@ class Command(BaseCommand):
     def read_from_file(self, path: str):
         with open(path, encoding="utf-8") as file:
             queries = file.readlines()
-        self.queries = concat(queries)
+        queries = concat(queries)
+        queries = filter(lambda query: "INSERT INTO" in query, queries)
+        queries = list(map(lambda query: reconstruct_insert(query), queries))
+        self.queries = queries
 
-    def execute_all_queries(self) -> int:
-        fail_count = 0
+    def execute_all_queries(self) -> List[str]:
+        failing = []
+        _tqdm = tqdm(self.queries, desc="Execution")
         with connection.cursor() as cursor:
-            for i, query in enumerate(tqdm(self.queries, desc="Execution")):
+            for i, query in enumerate(_tqdm):
                 query = query.strip()
                 try:
                     cursor.execute(query)
                 except Exception as e:
-                    fail_count += 1
-                    # if "CREATE UNIQUE INDEX" in str(e):
-                    #     continue
-                    # if "CREATE INDEX" in str(e):
-                    #     continue
-                    # if "already exists" in str(e):
-                    #     continue
-                    # if "UNIQUE constraint failed" in str(e):
-                    #     continue
-                    # log.error(f"Query {i} failed: {e}: {query}")
-                # else:
-                #     if "CREATE TABLE" in query:
-                #         continue
-                #     if "CREATE INDEX" in query:
-                #         continue
-                #     log.info(query)
-        return fail_count
+                    failing.append(query)
+        sleep(1)
+        _tqdm.clear()
+        _tqdm.close()
+        return failing
 
     def handle(self, *args, **options):
         management.call_command("migrate")
         self.read_from_file(options["dump_path"])
-        for i in range(10):
-            print(f"Iteration {i + 1}")
-            fail_count = self.execute_all_queries()
-            if fail_count != 0:
-                log.error(f"Fail count: {fail_count}")
+        failing = []
+        last_count = 0
+        fail_count = None
+        i = 1
+        while fail_count != last_count:
+            print(f"\n\nIteration {i}")
+            last_count = fail_count
+            failing = self.execute_all_queries()
+            fail_count = len(failing)
+            if fail_count > 0:
+                log.warning(f"Fail count: {wrap(fail_count)}, which is ~{100 * fail_count / len(self.queries):.0f}%")
+            i += 1
+        log.info(f"Converged. Failing queries:")
+        for failed in failing:
+            log.warning(failed)
+        with connection.cursor() as cursor:
+            cursor.execute("DELETE FROM django_session;")
