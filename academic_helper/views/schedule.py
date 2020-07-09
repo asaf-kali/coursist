5be5a045b9981ed2215e421c1a8be52f2aabb09a
Nadav Goldin <ngoldin@virtual-gate.net>
2020-07-09 14:33:08 +0300

    backend: add API call to export calendar in ICS format
    The new route is at: /schedule, with query parameter `download`:
    1. /schedule/?download=ical
    Will download the existing user scheduled courses in ICS format
    (iCalendar)
    
    2. /schedule/?download=json
    Will download the existing user scheduled courses in JSON format
    but not as repeating events (just the canonical dates)
    
    The ICS format can be easily exported to google calendar, outlook, etc.
    
    Signed-off-by: Nadav Goldin <ngoldin@virtual-gate.net>

--- academic_helper/views/schedule.py
+++ academic_helper/views/schedule.py
@@ -1,8 +1,9 @@
 import json
-
+from io import BytesIO
 from django.db.models import Q
 from django.http import JsonResponse, HttpResponseBadRequest
-
+from django.http import FileResponse
+from django.core.serializers.json import DjangoJSONEncoder
 from academic_helper.logic.errors import UserNotLoggedInError, CourseNotFoundError
 from academic_helper.logic.schedule import (
     set_user_schedule_group,
@@ -10,9 +11,14 @@ from academic_helper.logic.schedule import (
     get_all_classes,
     del_user_schedule_groups,
 )
+
+from academic_helper.logic.schedule_export import ScheduleExport
 from academic_helper.models.course import Course
 from academic_helper.views.basic import ExtendedViewMixin
+import logging
+import arrow
 
+LOG = logging.getLogger(__name__)
 SCHEDULE_COOKIE = "schedule"
 
 
@@ -30,7 +36,25 @@ class ScheduleView(ExtendedViewMixin):
             # get_context_data, which already used this cookie
             response.delete_cookie(SCHEDULE_COOKIE)
 
-        return response
+        if request.GET.get("download") == "ical":
+            ical = ScheduleExport(user=self.user).as_ical()
+            buffer = BytesIO()
+            buffer.writelines([line.encode() for line in ical])
+            buffer.seek(0)
+            return FileResponse(
+                buffer, as_attachment=True, filename=f"coursist_export_{self.user}_{arrow.utcnow()}.ics",
+            )
+        elif request.GET.get("download") == "json":
+            event_json = ScheduleExport(user=self.user).as_dict()
+            buffer = BytesIO()
+            buffer.write(json.dumps(event_json, indent=4, sort_keys=True, cls=DjangoJSONEncoder).encode())
+            buffer.seek(0)
+            return FileResponse(
+                buffer, as_attachment=True, filename=f"coursist_export_{self.user}_{arrow.utcnow()}.json",
+            )
+
+        else:
+            return response
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -50,7 +74,7 @@ class ScheduleView(ExtendedViewMixin):
             Q(name__icontains=search_val) | Q(course_number__icontains=search_val)
         ).order_by("course_number")[:10]
         serialized = [c.as_dict for c in courses]
-        return JsonResponse({"status": "success", "courses": serialized}, json_dumps_params={"ensure_ascii": False})
+        return JsonResponse({"status": "success", "courses": serialized}, json_dumps_params={"ensure_ascii": False},)
 
     def get_classes(self, course_number):
         if not course_number.isdigit():
@@ -58,7 +82,7 @@ class ScheduleView(ExtendedViewMixin):
         try:
             groups = get_all_classes(course_number)
             return JsonResponse({"status": "success", "groups": groups})
-        except CourseNotFoundError as e:
+        except CourseNotFoundError:
             return JsonResponse({"status": "error", "message": "Course not found"})
 
     def on_user_group_choice(self, choice):
@@ -81,13 +105,13 @@ class ScheduleView(ExtendedViewMixin):
         if "search_val" in request.POST:
             search_val = request.POST["search_val"]
             return self.on_search_course(search_val)
-        if "course_number" in request.POST:
+        elif "course_number" in request.POST:
             course_number = request.POST["course_number"]
             return self.get_classes(course_number)
-        if "group_choice" in request.POST:
+        elif "group_choice" in request.POST:
             choice = request.POST["group_choice"]
             return self.on_user_group_choice(choice)
-        if "groups_to_del" in request.POST:
+        elif "groups_to_del" in request.POST:
             choices = json.loads(request.POST["groups_to_del"])["group_ids"]
             return self.on_user_delete_groups(choices)
         else:
