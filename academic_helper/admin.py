9ef0a53f650fa735c80a5bd82d24fd1a88fb734f
Asaf Kali <akali93@cs.huji.ac.il>
2020-08-11 18:57:13 +0300

    - Change in github workflows. - Fix tests. - Additions to admin console.

--- academic_helper/admin.py
+++ academic_helper/admin.py
@@ -21,7 +21,7 @@ from academic_helper.models import (
 
 class CoursistUserAdmin(admin.ModelAdmin):
     search_fields = ["pk", "username", "email", "first_name", "last_name"]
-    list_display = ["username", "email", "date_joined"]
+    list_display = ("username", "email", "date_joined")
 
 
 admin.site.register(CoursistUser, CoursistUserAdmin)
@@ -43,13 +43,15 @@ admin.site.register(Department, DepartmentAdmin)
 
 class CourseAdmin(admin.ModelAdmin):
     search_fields = ["pk", "course_number", "name"]
+    list_display = ("course_number", "name")
 
 
 admin.site.register(Course, CourseAdmin)
 
 
 class CourseOccurrenceAdmin(admin.ModelAdmin):
-    search_fields = ["pk", "course__name", "name", "year", "semester"]
+    search_fields = ["pk", "course__name", "year", "semester"]
+    list_display = ("course", "year", "semester")
 
 
 admin.site.register(CourseOccurrence, CourseOccurrenceAdmin)
@@ -78,6 +80,10 @@ admin.site.register(Teacher, TeacherAdmin)
 
 class ClassGroupAdmin(admin.ModelAdmin):
     search_fields = ["pk", "mark", "occurrence__course__name", "class_type"]
+    list_display = ("mark", "course_name", "year", "semester", "class_type")
+
+    def course_name(self, obj: ClassGroup):
+        return obj.occurrence.course.name
 
 
 admin.site.register(ClassGroup, ClassGroupAdmin)
@@ -85,6 +91,13 @@ admin.site.register(ClassGroup, ClassGroupAdmin)
 
 class CourseClassAdmin(admin.ModelAdmin):
     search_fields = ["teacher__name", "group__occurrence__course__name"]
+    list_display = ("pk", "course_name", "start_time", "end_time", "year", "semester", "group_mark", "teacher")
+
+    def course_name(self, obj: CourseClass):
+        return obj.group.occurrence.course.name
+
+    def group_mark(self, obj: CourseClass):
+        return obj.group.mark
 
 
 admin.site.register(CourseClass, CourseClassAdmin)
@@ -92,6 +105,7 @@ admin.site.register(CourseClass, CourseClassAdmin)
 
 class ClassScheduleAdmin(admin.ModelAdmin):
     search_fields = ["user__username", "group__occurrence__course__name"]
+    list_display = ("user", "group")
 
 
 admin.site.register(ClassSchedule, ClassScheduleAdmin)
@@ -105,7 +119,8 @@ admin.site.register(StudyBlock, StudyBlockAdmin)
 
 
 class CompletedCourseAdmin(admin.ModelAdmin):
-    search_fields = ["course__name", "block__name", "user__username"]
+    search_fields = ["course__name", "block__name", "user__username", "user__email"]
+    list_display = ("user", "block", "course")
 
 
 admin.site.register(UserCourseChoice, CompletedCourseAdmin)
@@ -121,6 +136,7 @@ admin.site.register(DegreeProgram, DegreeProgramAdmin)
 class RatingDummyAdmin(admin.ModelAdmin):
     search_fields = ["name", "object_id"]
     readonly_fields = ["content_object", "object_id"]
+    list_display = ("name", "content_object", "score")
 
 
 admin.site.register(RatingDummy, RatingDummyAdmin)
